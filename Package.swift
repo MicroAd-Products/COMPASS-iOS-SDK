d327a0ca420a3b68883d6667ea217a949f0e5c6c
suyama-naoki <suyama_naoki@microad.co.jp>
2024-08-23 15:11:28 +0900

    Fix Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CompassIosSdk",
-            path: "./CompassIosSdk.xcframework"
+            path: "CompassIosSdk.xcframework"
         )
     ]
 )
